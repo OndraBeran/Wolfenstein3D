b7c4604d0ae2e1791d5ebba52618e6b711fcb635
OndraBeran2 <ondrej.beran@student.gyarab.cz>
2022-10-03 11:20:27 +0200

    ano

---
+++ src/model/Map.java
@@ -0,0 +1,16 @@
+package model;
+
+public class Map {
+    final static boolean[][] map = new boolean[][]{
+            {true, true, true, true, true, true, true, true, true, true},
+            {true, false, false, false, false, false, false, false, false, true},
+            {true, false, false, false, false, false, false, false, false, true},
+            {true, false, false, false, false, false, false, false, false, true},
+            {true, false, false, false, false, false, false, false, false, true},
+            {true, false, false, false, false, false, false, false, false, true},
+            {true, false, false, false, false, false, false, false, false, true},
+            {true, false, false, false, false, false, false, false, false, true},
+            {true, false, false, false, false, false, false, false, false, true},
+            {true, true, true, true, true, true, true, true, true, true}
+    };
+}
